766fd863bae7f258a10f8f5ad27d189453bcf334
Jayant Malik <dev.jayantmalik@gmail.com>
2020-11-05 11:00:47 +0530

    add graphql query to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
+import { graphql, PageProps } from "gatsby";
 import React from "react";
-export interface IndexProps {}
+export type IndexProps = {} & PageProps;
 
-const Index: React.FC<IndexProps> = () => {
-  return <div>Root Page of site.</div>;
+const Index: React.FC<IndexProps> = ({ data }) => {
+  return (
+    <div>
+      <h1>Root Page of site.</h1>
+      <p>Site Title: {(data as any).site.siteMetadata.title}</p>
+      <p>Site Description: {(data as any).site.siteMetadata.description}</p>
+      <p>Site Author: {(data as any).site.siteMetadata.author}</p>
+    </div>
+  );
 };
+
+export const query = graphql`
+  query SiteQuery {
+    site(siteMetadata: {}) {
+      siteMetadata {
+        title
+        author
+        description
+      }
+    }
+  }
+`;
 
 export default Index;
